0f05c338c4a8b785ae13ba0909e015911d39c54e
Mike <mikeivanov2010@gmail.com>
2026-08-19 16:05:04 -0400

    final updated code

--- Arduino-robo-hand/Arduino-robo-hand.ino
+++ Arduino-robo-hand/Arduino-robo-hand.ino
@@ -10,19 +10,19 @@ const int stepDelay = 20;
 
 void setup() {
   servoA.attach(servoAPin);
-  servoA.write(90);    // starting position
+  servoA.write(0);     // starting position
 
   servoB.attach(servoBPin);
   servoB.write(90);    // starting position
 
   delay(300); // let both servos settle before moving
 
-  // --- Servo A: 90 -> 0 -> 90 ---
-  for (int pos = 90; pos >= 0; pos--) {
+  // --- Servo A: 0 -> 180 -> 0 ---
+  for (int pos = 0; pos <= 180; pos++) {
     servoA.write(pos);
     delay(stepDelay);
   }
-  for (int pos = 0; pos <= 90; pos++) {
+  for (int pos = 180; pos >= 0; pos--) {
     servoA.write(pos);
     delay(stepDelay);
   }
